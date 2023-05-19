ca980c232c76d82f98619be729ea4f77d4cb1d3f
VioletaNaboy <vnaboishchykova@gmail.com>
2023-05-20 00:21:59 +0200

    task_2

---
+++ src/js/02-timer.js
@@ -0,0 +1,60 @@
+import flatpickr from 'flatpickr';
+import Notiflix from 'notiflix';
+import 'flatpickr/dist/flatpickr.min.css';
+const btnStart = document.querySelector('button[data-start]');
+const elInput = document.querySelector('input#datetime-picker');
+btnStart.disabled = true;
+const selectedDates = [];
+const flatpickrInstance = flatpickr(elInput, {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    if (selectedDates[0] > new Date()) {
+      btnStart.disabled = false;
+    } else {
+      btnStart.disabled = true;
+      Notiflix.Notify.failure('Please choose a date in the future');
+    }
+    return selectedDates[0];
+  },
+});
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  return { days, hours, minutes, seconds };
+}
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+btnStart.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    const selectedDate = flatpickrInstance.selectedDates[0];
+    const currentDate = new Date();
+    const timeRemaining = convertMs(
+      selectedDate.getTime() - currentDate.getTime()
+    );
+    if (timeRemaining.seconds >= 0) {
+      document.querySelector('[data-days]').textContent = `${addLeadingZero(
+        timeRemaining.days
+      )}`;
+      document.querySelector('[data-hours]').textContent = `${addLeadingZero(
+        timeRemaining.hours
+      )}`;
+      document.querySelector('[data-minutes]').textContent = `${addLeadingZero(
+        timeRemaining.minutes
+      )}`;
+      document.querySelector('[data-seconds]').textContent = `${addLeadingZero(
+        timeRemaining.seconds
+      )}`;
+    }
+  }, 1000);
+  btnStart.disabled = true;
+});
